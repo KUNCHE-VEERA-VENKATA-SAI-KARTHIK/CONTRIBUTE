95f87e75a5d3350d47610d14b3d849234dedf169
KUNCHE-VEERA-VENKATA-SAI-KARTHIK <110412373+KUNCHE-VEERA-VENKATA-SAI-KARTHIK@users.noreply.github.com>
2023-12-30 21:05:08 +0530

    Time: 76 ms (5.15%) | Memory: 24.1 MB (62.08%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool check(string s, int i, int j) {
+        while (i < j) {
+            if (s[i] == s[j]) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s[i] == s[j]) {
+                i++;
+                j--;
+            } else if (s[i] != s[j]) {
+                // Check right: remove right
+                if (check(s, i, j - 1))
+                    return true;
+                // Check left: remove left
+                else if (check(s, i + 1, j))
+                    return true;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+};
