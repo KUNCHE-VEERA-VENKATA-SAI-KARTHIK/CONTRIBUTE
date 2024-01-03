91287a0feccf5585c490081f4b65f4eafc7f1804
KUNCHE-VEERA-VENKATA-SAI-KARTHIK <110412373+KUNCHE-VEERA-VENKATA-SAI-KARTHIK@users.noreply.github.com>
2024-01-03 17:44:24 +0530

    Time: 3 ms (97.10%) | Memory: 7.7 MB (44.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-      sort( s.begin(),s.end() );
-      sort( t.begin(),t.end());
+        int freq[256]={0};
 
-       if(s==t)
-       return true;
-       else 
-       return false;
+        for( int  i=0; i< s.size(); i++)
+        {
+            freq[s[i]]++;
+        }
+        for(int i =0 ;i<t.size();i++)
+        {
+          freq[t[i]]--;
+        }
 
-        
+        for( int i=0; i<256;i++)
+
+{
+    if(freq[i]!=0)
+    return false;
+}      
+return true; 
     }
 };
