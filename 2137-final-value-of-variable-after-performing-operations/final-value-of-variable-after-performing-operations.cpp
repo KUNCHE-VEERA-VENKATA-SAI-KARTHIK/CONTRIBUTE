32e1276733beb4148ca0fbd55624baf758ca8c4f
KUNCHE-VEERA-VENKATA-SAI-KARTHIK <110412373+KUNCHE-VEERA-VENKATA-SAI-KARTHIK@users.noreply.github.com>
2024-01-23 23:36:45 +0530

    Time: 10 ms (26.58%) | Memory: 17.6 MB (11.84%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+
+        for (auto i : operations)
+        {
+        if(i=="X++")
+        x++;
+        else if(i=="++X")
+        ++x;
+        else if(i=="--X")
+        --x;
+        else if(i=="X--")
+        x--;
+        }
+        return x;
+
+
+        
+    }
+};
