8a03d518ca598a35a3152fb82e129fcac5797535
KUNCHE-VEERA-VENKATA-SAI-KARTHIK <110412373+KUNCHE-VEERA-VENKATA-SAI-KARTHIK@users.noreply.github.com>
2024-01-24 23:43:47 +0530

    Time: 10 ms (25.11%) | Memory: 23.4 MB (6.36%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+
+        sort(nums.begin(),nums.end());
+        
+        for(auto k : nums)
+        {
+            cout<<k<<",";
+        }
+     
+        int n=nums.size();
+        int count=0;
+        int i;
+        int j;
+
+
+
+        for( i=0; i< n; i++)
+        {
+            // if(nums[i]>target)
+            //     break;
+            
+
+            for (j=i+1; j<n ; j++)
+            {
+                // if(nums[j]>target)
+                // break;
+                if(nums[i]+nums[j]<target)
+                {
+                    count++;
+                }
+
+            }
+
+        }
+        
+        return count;
+        
+    }
+};
