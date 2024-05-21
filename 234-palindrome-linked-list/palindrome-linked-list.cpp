3b5ed3b8cbc5061db5b88230783f69e37bb8cc56
KUNCHE-VEERA-VENKATA-SAI-KARTHIK <110412373+KUNCHE-VEERA-VENKATA-SAI-KARTHIK@users.noreply.github.com>
2024-05-21 21:58:54 +0530

    Time: 176 ms (44.26%) | Memory: 120.5 MB (55.10%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,29 +10,50 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
     bool isPalindrome(ListNode* head) {
-        vector<int> s;
-        ListNode * temp = head;
-
-        while(temp!=NULL)
-        {   s.push_back(temp->val);
-            temp=temp->next;
-        }
-        int i =0;
-        int j =s.size()-1;
-
-        while(i<=j)
-        {
-            if(s[i]!=s[j])
-            return false;
-
-            i++;
-            j--;
-
+        if (head == nullptr || head->next == nullptr) {
+            return true;
         }
 
-        return true;
+        // Find the middle of the list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
 
-        
+        // Reverse the second half of the list
+        ListNode* secondHalfStart = reverse(slow);
+
+        // Compare the first and the second half
+        ListNode* firstHalfStart = head;
+        ListNode* secondHalfIter = secondHalfStart;
+        bool palindrome = true;
+        while (secondHalfIter != nullptr) {
+            if (firstHalfStart->val != secondHalfIter->val) {
+                palindrome = false;
+                break;
+            }
+            firstHalfStart = firstHalfStart->next;
+            secondHalfIter = secondHalfIter->next;
+        }
+
+        // Restore the list (optional)
+        reverse(secondHalfStart);
+
+        return palindrome;
     }
 };
